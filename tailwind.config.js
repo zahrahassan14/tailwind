08c5a9c0974ba8f3aff36501eab6a2ef8dbbacec
zahrahassan14 <zahrahassan7272@gmail.com>
2024-08-09 17:30:49 +0500

    tailwindcss landing page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        navy: '#001f3f',
+        yellow: '#FFDC00',
+        gray: {
+          300: '#d1d5db',
+          400: '#9ca3af',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
